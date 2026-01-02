7be9af2405bb28a924746c0233d51745c216029a
abinayar2711-sketch <abinayar2711@gmail.com>
2026-01-02 17:37:57 +0530

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,4 @@
-ns & import { useState } from 'react';
+import { useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { useMutation } from '@tanstack/react-query';
 import { Button } from '@/components/ui/button';
