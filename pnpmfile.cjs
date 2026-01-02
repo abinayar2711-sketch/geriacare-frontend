9903bc1dd9a42d72a965a12155a324d41ec01a5e
abinayar2711-sketch <abinayar2711@gmail.com>
2026-01-02 18:42:07 +0530

    Delete pnpmfile.cjs

--- pnpmfile.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  hooks: {
-    readPackage(pkg) {
-      pkg.dependencies = pkg.dependencies || {};
-      pkg.dependencies['@supabase/supabase-js'] = '^2.39.7';
-      return pkg;
-    }
-  }
-};
